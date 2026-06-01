4c475c7b51990fef236b7a285ae9b3deb1ec5f31
Kostiantyn <kostyantyn.komarov@droid-technologies.com>
2026-06-01 15:50:42 +0100

    Added to publish the list of phase instances.

---
+++ Backend/Libs/Include/Logic/PhasesList.hpp
@@ -0,0 +1,9 @@
+#ifndef __LOGIC_PHASE_PHASES_LIST_HPP
+#define __LOGIC_PHASE_PHASES_LIST_HPP
+
+#include "PhaseMotorLeak.hpp"
+#include "PhaseMotorResist.hpp"
+#include "PhaseSensLeak.hpp"
+#include "PhaseSensResist.hpp"
+
+#endif // #ifndef __LOGIC_PHASE_PHASES_LIST_HPP
