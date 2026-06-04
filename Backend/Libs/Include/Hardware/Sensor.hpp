4868f501c40f3d096e542369e3f1b3f3ead43503
Kostiantyn <kostyantyn.komarov@droid-technologies.com>
2026-06-04 13:46:29 +0100

    Updated for making solid architecture.

--- Backend/Libs/Include/Hardware/Sensor.hpp
+++ Backend/Libs/Include/Hardware/Sensor.hpp
@@ -1,10 +1,39 @@
 #ifndef __HARDWARE_SENSOR_HPP
 #define __HARDWARE_SENSOR_HPP
 
-class Sensor
+#include <cstdint>
+#include "Node.hpp"
+#include "ResultCode.hpp"
+
+class Sensor : public Node
 {
 public:
-    
+    /**
+     * @brief 
+     * @param 
+     */
+    Sensor(const char *kpPath) :
+            Node(kpPath)
+    {}
+
+    /**
+     * @brief 
+     */
+    virtual ~Sensor(void)
+    {}
+
+    /**
+     * @brief  
+     * @return 
+     */
+    virtual float readF(void)
+    {
+        return .0f;
+    }
+
+    virtual ResultCode::Index_t configure(const void *) = 0;
+
+    virtual int32_t read(void) = 0;
 };
 
 #endif // #ifndef __HARDWARE_SENSOR_HPP
